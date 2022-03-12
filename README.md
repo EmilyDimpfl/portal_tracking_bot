6286faee3aabe911a3bd3508a198312c491fb1c8
Emily Dimpfl <emily.dimpfl@gmail.com>
2022-03-12 02:15:21 -0800

    We now have a basic discord bot.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # portal_tracking_bot
+
 Portal use and payment tracking bot for the Echo server Dimensions portal discord for the MMORPG Dofus.
+
+## Setup
+
+In order to create a bot account to add to your server, follow the instructions at https://discordpy.readthedocs.io/en/stable/discord.html#discord-intro
+
+You can manage your bots on Discord's application developer page: https://discord.com/developers/applications
+
+Once you have set up the bot, you'll want to add your discord bot token to your .env file.
+
+Copy the `env.dev` file to `.env`, and change the `DISCORD_TOKEN=...` to `DISCORD_TOKEN=YOURTOKEN` (where `YOURTOKEN` is the token on the bot application page, obviously). 
+
+## Running
+
+Run `portalbot.py`.
--- portalbot.py
+++ portalbot.py
@@ -1,9 +1,63 @@
 #!/usr/bin/env python3
+
+import logging
+import os
+from dotenv import load_dotenv
 import discord
+# Configure logging for both logging to a file and logging to stdout.
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("debug.log"),
+        logging.StreamHandler()
+    ]
+)
+
+client = discord.Client()  # create our client
+
+
+@client.event
+async def on_ready():
+    """
+    event when we log in
+    """
+    logging.info('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    """
+    event when someone sends a message in a channel
+    """
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
 
 def main():
-    # todo: the whole thing
-    pass
+    """
+    runs our bot
+    """
+    load_dotenv()  # load our .env file into environment variables
+    # this is used to read our DISCORD_TOKEN in without having
+    # the token committed to the repository.
+
+    # Here's how you make a bot:
+    # https://discordpy.readthedocs.io/en/stable/discord.html#discord-intro
+
+    # Here's discord.py's quick start:
+    # https://discordpy.readthedocs.io/en/stable/quickstart.html
+
+    # Here's discord.py's documentation:
+    # https://discordpy.readthedocs.io/en/stable/index.html
+
+    # run the client:
+    client.run(os.environ['DISCORD_TOKEN'])
+    # this blocks until we press Ctrl-C
+
 
 if __name__ == "__main__":
     main()
